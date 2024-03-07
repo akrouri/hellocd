d00c57c6e0b251aa311d4e8326c0fc2a459ae336
akrouri <akrouri@free.fr>
2024-03-07 03:02:23 +0100

    committing again changes to pom.xml and HellocdApplication.java

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -7,14 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
-@EnableAutoConfiguration
 public class HellocdApplication {
 
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello Continuous Delivery!";
+		return "Hello World!";
 	}
 
 	public static void main(String[] args) {
